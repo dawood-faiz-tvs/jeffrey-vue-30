3f84d7cdf8db4ee603080601b034e06f2a9f4ab9
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-07-08 20:58:56 +0500

    day 18
    learned about composition API way of doing things

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let message = ref('HELLO THERE...')
+
+let changeValue = () => {
+  message.value = 'HELLO WORLD!'
+}
 </script>
 
 <template>
   <main>
+    {{ message }}
+    <button type="button" @click="changeValue">CLICK ME</button>
     <TheWelcome />
   </main>
 </template>
